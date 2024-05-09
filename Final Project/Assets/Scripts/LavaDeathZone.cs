95602fc519a6e0d3e27191836d5b9493ee85970f
KingA <70245645+Pwncakee@users.noreply.github.com>
2024-05-08 19:33:21 -0700

    Added sound effects for death

--- Final Project/Assets/Scripts/LavaDeathZone.cs
+++ Final Project/Assets/Scripts/LavaDeathZone.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class LavaDeathZone : MonoBehaviour
 {
     public Transform respawnPoint;
+    public AudioClip deathSound; // The sound that will play upon death
+    private AudioSource audioSource; // The source from which the sound will play
     private string[] deathTags = { "Player", "death", "lava" };
+
+    void Start()
+    {
+        // Get the AudioSource component attached to the same GameObject
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -12,10 +20,13 @@ public class LavaDeathZone : MonoBehaviour
             if (other.gameObject.CompareTag(tag))
             {
                 other.transform.position = respawnPoint.position;
+                
+                audioSource.PlayOneShot(deathSound);
                 break;
             }
         }
     }
 }
+
 
 
